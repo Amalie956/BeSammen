88443d4f38adbbd62336f1a89a2ed53365bbaadc
Amalie956 <amal956e@stud.kea.dk>
2023-05-12 09:47:11 +0200

    Lavet struktur via mapperne ui, domain og database

---
+++ app/src/main/java/com/example/besammen/Domain/MainActivityk.java
@@ -0,0 +1,16 @@
+package com.example.besammen.Domain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.besammen.R;
+
+public class MainActivityk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityk);
+    }
+}
