ed7b24368ced1b56c2be14ed2753596663a980df
Amalie956 <amal956e@stud.kea.dk>
2023-05-17 18:27:27 +0200

    test commit

---
+++ app/src/main/java/com/example/besammen/domain/UserService.java
@@ -0,0 +1,58 @@
+package com.example.besammen.domain;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.besammen.ui.UserLogin;
+import com.example.besammen.ui.UserRegistration;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserService {
+    private Context context;
+    private FirebaseAuth firebaseAuth;
+
+    public UserService(Context context) {
+        this.context = context;
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void loginMethod(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(context, UserRegistration.class);
+                            context.startActivity(intent);
+                            // Finish the current activity if needed
+                            // ((Activity) context).finish();
+                        } else {
+                            Toast.makeText(context, "Login Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Register Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(context, UserLogin.class);
+                            context.startActivity(intent);
+                            // Finish the current activity if needed
+                            // ((Activity) context).finish();
+                        } else {
+                            Toast.makeText(context, "Register Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/besammen/ui/UserRegistration.java
@@ -0,0 +1,96 @@
+package com.example.besammen.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.besammen.R;
+import com.example.besammen.domain.UserService;
+import com.example.besammen.domain.UserValidator;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class UserRegistration extends AppCompatActivity {
+    String[] items ={"18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"};
+    String[] gender ={"Kvinde", "Mand", "Andet"};
+    String[] diagnosis ={"ADHD"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItem;
+
+    UserService userLoginService = new UserService(this);
+    UserValidator userLoginValidator = new UserValidator(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+
+        //age dropdown
+        autoCompleteTextView = findViewById(R.id.age);
+        adapterItem = new ArrayAdapter<String>(this, R.layout.dropdown_lists, items);
+        autoCompleteTextView.setAdapter(adapterItem);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+        });
+
+        //gender dropdown
+        autoCompleteTextView = findViewById(R.id.gender);
+        adapterItem = new ArrayAdapter<String>(this, R.layout.dropdown_lists, gender);
+        autoCompleteTextView.setAdapter(adapterItem);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+        });
+
+        //diagnosis dropdown
+        autoCompleteTextView = findViewById(R.id.diagnosis);
+        adapterItem = new ArrayAdapter<String>(this, R.layout.dropdown_lists, diagnosis);
+        autoCompleteTextView.setAdapter(adapterItem);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+        });
+
+        TextInputEditText editTextEmail = findViewById(R.id.email);
+        TextInputEditText editTextPassword = findViewById(R.id.password);
+        Button buttonReg = findViewById(R.id.btn_register);
+        TextView textView = findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserRegistration.this, UserLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                boolean x = userLoginValidator.UserValidator(email, password);
+
+                if (x == true) {
+                    userLoginService.signIn(email, password);
+                }
+            }
+        });
+    }
+}
