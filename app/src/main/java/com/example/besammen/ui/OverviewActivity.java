88c49cef4a148ccad3fb5b66c07130e5d2a91865
Mahe0055 <mahe00555@stud.kea.dk>
2023-05-17 15:30:36 +0200

    Overview frontpage layout

---
+++ app/src/main/java/com/example/besammen/ui/OverviewActivity.java
@@ -0,0 +1,16 @@
+package com.example.besammen.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.besammen.R;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview_app_activities);
+    }
+}
