c8c324a5d2ceb26022050b615ad94f93d54913f1
Klara <Klara.blauenfeldt@gmail.com>
2023-05-18 11:49:35 +0200

    profil

---
+++ app/src/main/java/com/example/besammen/ui/UserProfile.java
@@ -0,0 +1,16 @@
+package com.example.besammen.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.besammen.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
