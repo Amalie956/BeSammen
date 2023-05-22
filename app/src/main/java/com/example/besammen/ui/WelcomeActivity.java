70c0c0e1dea5663178c9a80ee99ed155f66431a3
Abdu1RH <abdul.rh2403@gmail.com>
2023-05-22 10:06:10 +0200

    Message class and activity message layout commit'ed

--- app/src/main/java/com/example/besammen/ui/WelcomeActivity.java
+++ app/src/main/java/com/example/besammen/ui/WelcomeActivity.java
@@ -2,26 +2,36 @@ package com.example.besammen.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.besammen.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-    ImageView imageView;
+    //ImageView imageView;
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        imageView = findViewById(R.id.imageView);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        //imageView = findViewById(R.id.imageView);
+
+        //button = findViewById(R.id.continuebtn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+        //imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //Når man trykker på billedet, kommer man hen til Overview siden
-                Intent intent = new Intent(WelcomeActivity.this, OverviewActivity.class);
+                Intent intent = new Intent(WelcomeActivity.this, Message.class);
                 startActivity(intent);
             }
         });
