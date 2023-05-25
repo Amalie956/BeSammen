545ab7968a31826ecacae2b1f71e9b1dc70761bf
Amalie956 <amal956e@stud.kea.dk>
2023-05-25 19:37:08 +0200

    Tilføjet en repository mappe hvor firebase ligger og lagt tilhørende logik derover + ændret metodenavne

--- app/src/main/java/com/example/besammen/domain/User.java
+++ app/src/main/java/com/example/besammen/domain/User.java
@@ -1,12 +1,15 @@
 package com.example.besammen.domain;
 public class User {
-
+    private String email;
+    private String password;
     private String userName;
     private String gender;
-    private int age;
+    private String age;
     private String diagnosis;
 
-    public User(String userName, String gender, int age, String diagnosis) {
+    public User(String email, String password, String userName, String gender, String age, String diagnosis) {
+        this.email = email;
+        this.password = password;
         this.userName = userName;
         this.gender = gender;
         this.age = age;
@@ -17,31 +20,23 @@ public class User {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getAge() {
+    public String getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getDiagnosis() {
         return diagnosis;
     }
 
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
--- app/src/main/java/com/example/besammen/domain/UserService.java
+++ app/src/main/java/com/example/besammen/domain/UserService.java
@@ -1,14 +1,12 @@
 package com.example.besammen.domain;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-
+import com.example.besammen.repository.UserRepository;
 import com.example.besammen.ui.OverviewActivity;
-import com.example.besammen.ui.UserLogin;
-import com.example.besammen.ui.UserRegistration;
 import com.example.besammen.ui.WelcomeActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,15 +14,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserService {
+    private UserValidatorService userLoginValidator;
     private Context context;
     private FirebaseAuth firebaseAuth;
 
     public UserService(Context context) {
         this.context = context;
+        this.userLoginValidator = new UserValidatorService(context);
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    public void loginMethod(String email, String password) {
+    public void userSignIn(String email, String password) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
@@ -41,22 +41,31 @@ public class UserService {
                 });
     }
 
-    public void createUserMethod(String email, String password) {
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            //Toast.makeText(context, "Register Successful", Toast.LENGTH_SHORT).show();
-                            //Intent intent = new Intent(context, WelcomeActivity.class);
-                            //context.startActivity(intent);
-                            // Finish the current activity if needed
-                            // ((Activity) context).finish();
-                        } else {
-                            Toast.makeText(context, "Fejl i oprettelse af bruger. Prøv igen", Toast.LENGTH_SHORT).show();
+    public void createUser(User user) {
+        boolean isUserRegistrationValid = userLoginValidator.createUserValidator(user);
+        if (isUserRegistrationValid) {
+            firebaseAuth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                UserRepository userRepository = new UserRepository();
+                                userRepository.storeUserDataInFirestoreDatabase(user);
+                                redirectToWelcomeActivity(user.getUserName(), user.getAge());
+                                ((Activity) context).finish();
+                            } else {
+                                Toast.makeText(context, "Fejl i oprettelse af bruger. Prøv igen", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+        }
+    }
+
+    public void redirectToWelcomeActivity(String username, String age) {
+        Intent intent = new Intent(context, WelcomeActivity.class);
+        intent.putExtra("userName", username);
+        intent.putExtra("age", age);
+        context.startActivity(intent);
     }
 
 }
